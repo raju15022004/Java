38fef443b33fc64d3ef007db13452cb380f7ad6e
raju15022004 <raju032004@gmail.com>
2025-09-21 21:02:02 +0530

    even.java

---
+++ flowchart/even.java
@@ -0,0 +1,15 @@
+// package flowchart;
+
+public class even {
+  public static void main(String[] args) {
+       int i=10;
+
+       while(i<=100){
+
+        System.out.println( i+" ");
+
+        i=i+1;
+       }
+  }
+
+}
