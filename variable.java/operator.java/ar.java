f1daaf55e15b10dd3c04cdad433e92c326e130e9
raju15022004 <raju032004@gmail.com>
2025-09-30 09:43:52 +0530

    ar.java

---
+++ variable.java/operator.java/ar.java
@@ -0,0 +1,15 @@
+// package operator.java;
+
+public class ar {
+  public static void main(String[] args) {
+      int A=10;
+      int B=5;
+
+      System.out.println("Add =" + (A+B));
+      System.out.println("Sub =" + (A-B));
+      System.out.println("multi="+(A*B));
+      System.out.println("divide="+(A/B));
+      
+  }
+
+}
