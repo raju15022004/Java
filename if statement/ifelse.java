aecd92dc15f3689ab5447e51ad650a0ee5f2d4ee
raju15022004 <raju032004@gmail.com>
2025-10-02 22:59:03 +0530

    ifelse.java

--- if statement/ifelse.java
+++ if statement/ifelse.java
@@ -1,13 +1,69 @@
-// package if statement;
 
-public class ifelse {
-    public static void main(String[] args) {
-        int age=18;
-        if(age>=18){
-          System.out.println("adult : drive,vote");
-        }
-        else{
-          System.out.println("not adult");
-        }
-    }
+
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int age=18;
+//         if(age>=18){
+//           System.out.println("adult : drive,vote");
+//         }
+//         else{
+//           System.out.println("not adult");
+//         }
+//     }
+// }
+
+
+/////////////////// largest ////////////////
+
+// public class ifelse{
+//   public static void main(String[] args) {
+//       int A=1;
+//       int B=5;
+
+//       if(A>=B){
+//         System.out.println("A is largest of 2");
+
+
+//       }
+//       else{
+//         System.out.println("B is largest of 2");
+//       }
+//   }
+// }
+
+
+////////////////////// odd even ///////////////////
+
+// public  class ifelse{
+//   public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);
+//       int number=sc.nextInt();
+
+//       if(number % 2 == 0){
+//         System.out.println("Even");
+//       }
+//       else{
+//         System.out.println("odd");
+//       }
+//   }
+// }
+
+
+//////////////////////// else /////////////////
+
+public class ifelse{
+  public static void main(String[] args) {
+      int age=16 ;
+
+      if(age>=18){
+        System.out.println("adult");
+
+      }
+      else if(age>=13 && age < 18){
+        System.out.println("teenager");
+      }
+      else{
+        System.out.println("child");
+      }
+  }
 }
