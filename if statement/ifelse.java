0babf0e89074c542937d7c7bd426fd0ae9ee41cc
raju15022004 <raju032004@gmail.com>
2025-10-02 22:07:40 +0530

    ifelse.java

---
+++ if statement/ifelse.java
@@ -0,0 +1,13 @@
+// package if statement;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int age=18;
+        if(age>=18){
+          System.out.println("adult : drive,vote");
+        }
+        else{
+          System.out.println("not adult");
+        }
+    }
+}
