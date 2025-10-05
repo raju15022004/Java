072512139e6d9f35fe259774a846c0e0ae7c1eaa
raju15022004 <raju032004@gmail.com>
2025-10-05 21:16:42 +0530

    whiledo.java

--- loop.java/whiledo.java
+++ loop.java/whiledo.java
@@ -1,7 +1,4 @@
 
-import java.util.Scanner;
-
-// public class whiledo {
 //   public static void main(String[] args) {
 //       int counter=0;
 //       while(counter<100){
@@ -28,18 +25,30 @@ import java.util.Scanner;
 
 ////////////////////// sum number ////////////
 
-public class whiledo{
+// public class whiledo{
+//   public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);
+//       int n =sc.nextInt();
+//       int sum=0;
+
+//       int i=1;
+//       while(i<=n){
+//         sum +=i;
+//         i++;
+//       }
+
+//       System.out.println("sum is :"+sum );
+//   }
+// }
+
+
+/////////////////// for loop ////////////////
+ public class whiledo{
   public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-      int n =sc.nextInt();
-      int sum=0;
+       int A=1;
 
-      int i=1;
-      while(i<=n){
-        sum +=i;
-        i++;
+      for(int i=1;i<=10;i++){
+        System.out.println("Raju");
       }
-
-      System.out.println("sum is :"+sum);
   }
-}
+ }
