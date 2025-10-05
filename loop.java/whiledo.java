bc256b4722ef35b0d2816a4c26b0a6ce9135c65d
raju15022004 <raju032004@gmail.com>
2025-10-05 22:15:34 +0530

    whiledo.java

--- loop.java/whiledo.java
+++ loop.java/whiledo.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 //   public static void main(String[] args) {
 //       int counter=0;
 //       while(counter<100){
@@ -43,12 +46,62 @@
 
 
 /////////////////// for loop ////////////////
- public class whiledo{
-  public static void main(String[] args) {
-       int A=1;
+//  public class whiledo{
+//   public static void main(String[] args) {
+//        int A=1;
 
-      for(int i=1;i<=10;i++){
-        System.out.println("Raju");
+//       for(int i=1;i<=10;i++){
+//         System.out.println("Raju");
+//       }
+//   }
+//  }
+
+////////////// square ///////////////
+
+// public class whiledo{
+//   public static void main(String[] args) {
+//       for(int i=1;i<=4;i++){
+//         System.out.println("****");
+//       }
+//   }
+// }
+
+
+/////////////// reverse //////////////
+
+// public class whiledo{
+//   public static void main(String[] args) {
+//       int n=10899;
+
+//       while(n>0){
+//         int lastDigit=n%10;
+//         System.out.print(lastDigit+" ");
+//         n=n/10;
+
+//       }
+//       System.out.println();
+//   }
+// }
+
+
+///////////////// prime number //////////////
+
+public class whiledo{
+  public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+
+      boolean isPrime=true;
+      for(int i=2;i<=n-1;i++){
+        if(n % i==0){
+          isPrime=false;
+        }
+      }
+      if(isPrime == true){
+        System.out.println("n is prime");
+      }
+      else{
+        System.out.println("n is not prime");
       }
   }
- }
+}
