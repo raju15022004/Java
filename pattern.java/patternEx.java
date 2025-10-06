a4706b824b3e9df035ab9a36aee81b711b538a33
raju15022004 <raju032004@gmail.com>
2025-10-06 19:30:36 +0530

    patternEx.java

--- pattern.java/patternEx.java
+++ pattern.java/patternEx.java
@@ -31,17 +31,36 @@
 
 ////////////// half pyramid ////////////
 
-public class patternEx{
+// public class patternEx{
+
+//   public static void main(String[] args) {
+
+//     int n=4;
+//     for(int i=1;i<=4;i++){
+//       for(int j=1;j<=i;j++){
+
+//         System.out.print(j);
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+
+/////////////// character ////////////
+ public class patternEx{
 
   public static void main(String[] args) {
-
     int n=4;
+     char  ch='A';
+
     for(int i=1;i<=4;i++){
       for(int j=1;j<=i;j++){
 
-        System.out.print(j);
+        System.out.print(ch);
+        ch++;
       }
       System.out.println();
     }
   }
-}
+ }
