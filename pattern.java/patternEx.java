8f4ee2f7a8faa52c862a1eac07d44e0a1a4ffd56
raju15022004 <raju032004@gmail.com>
2025-10-06 19:00:44 +0530

    patternEx.java

--- pattern.java/patternEx.java
+++ pattern.java/patternEx.java
@@ -1,10 +1,26 @@
-public class patternEx {
+// public class patternEx {
+
+//   public static void main(String[] args) {
+//     // int line=0;
+//     for(int line=1;line<=6;line++){
+//       for(int star=1;star<=line;star++){
+
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+//   }
+// }
+
+
+/////////// invered star //////////////
+
+public class patternEx{
 
   public static void main(String[] args) {
-    // int line=0;
-    for(int line=1;line<=6;line++){
-      for(int star=1;star<=line;star++){
-
+    int n=4;
+    for(int i=1;i<=4;i++){
+      for(int j=1;j<=(n-i+1);j++){
         System.out.print("*");
       }
       System.out.println("");
