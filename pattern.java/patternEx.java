0c5b296e32c0144fb936bc719dfc59175609707e
raju15022004 <raju032004@gmail.com>
2025-10-06 18:45:50 +0530

    patternEx.java

---
+++ pattern.java/patternEx.java
@@ -0,0 +1,13 @@
+public class patternEx {
+
+  public static void main(String[] args) {
+    // int line=0;
+    for(int line=1;line<=6;line++){
+      for(int star=1;star<=line;star++){
+
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
