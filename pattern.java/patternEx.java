87afcd0fa2d522b03de117c1e1edcef3c5d7c8a4
raju15022004 <raju032004@gmail.com>
2025-10-06 19:14:31 +0530

    patternEx.java

--- pattern.java/patternEx.java
+++ pattern.java/patternEx.java
@@ -15,15 +15,33 @@
 
 /////////// invered star //////////////
 
+// public class patternEx{
+
+//   public static void main(String[] args) {
+//     int n=4;
+//     for(int i=1;i<=4;i++){
+//       for(int j=1;j<=(n-i+1);j++){
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+//   }
+// }
+
+
+////////////// half pyramid ////////////
+
 public class patternEx{
 
   public static void main(String[] args) {
+
     int n=4;
     for(int i=1;i<=4;i++){
-      for(int j=1;j<=(n-i+1);j++){
-        System.out.print("*");
+      for(int j=1;j<=i;j++){
+
+        System.out.print(j);
       }
-      System.out.println("");
+      System.out.println();
     }
   }
 }
