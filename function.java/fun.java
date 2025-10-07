6d22e1bf5d1125f85d4479ba90deb95f46c613a0
raju15022004 <raju032004@gmail.com>
2025-10-07 18:52:06 +0530

    fun.java

--- function.java/fun.java
+++ function.java/fun.java
@@ -1,12 +1,40 @@
-public class fun {
+
+import java.util.Scanner;
+
+// public class fun {
+//   public static void printHelloWorld(){
+//     System.out.println("Hello World");
+//     System.out.println("Hello World");
+//     System.out.println("Hello World");
+//   }
+
+//   public static void main(String[] args) {
+//       printHelloWorld();
+//   }
+
+// }
+
+
+
+/////////////// parameter ////////////////
+
+public class fun{
   public static void printHelloWorld(){
     System.out.println("Hello World");
     System.out.println("Hello World");
     System.out.println("Hello World");
   }
 
+  public static void calculateSum(int num1,int num2){
+    int sum=num1+num2;
+    System.out.println("sum is " +sum);
+  }
+
   public static void main(String[] args) {
-      printHelloWorld();
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      calculateSum(a, b);
   }
 
 }
