41b15e2bb6d9fa8c91768d3bc1e961fa2e6500b0
raju15022004 <raju032004@gmail.com>
2025-10-07 18:40:34 +0530

    fun.java

---
+++ function.java/fun.java
@@ -0,0 +1,12 @@
+public class fun {
+  public static void printHelloWorld(){
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+
+  public static void main(String[] args) {
+      printHelloWorld();
+  }
+
+}
