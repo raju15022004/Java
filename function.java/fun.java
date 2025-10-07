33bdc2f32157266d59f5ea8a9e88af32cab93996
raju15022004 <raju032004@gmail.com>
2025-10-07 19:53:02 +0530

    fun.java

--- function.java/fun.java
+++ function.java/fun.java
@@ -66,6 +66,42 @@
 
 /////////////// factorial ///////////////
 
+// public class fun{
+
+//   public static void printHelloWorld(){
+//     System.out.println("Hello world");
+//     System.out.println("Hello world");
+//     System.out.println("Hello world");
+//   }
+
+//   public static void swap(int a, int b){
+//     int temp=a;
+//     a=b;
+//     b=temp;
+//   }
+
+//   public static int multiply(int a, int b){
+//     int product=a*b;
+//     return product;
+//   }
+
+//   public static int factorial(int n){
+//     int f=1;
+
+//     for(int i=1;i<=n;i++){
+//       f=f*i;
+//     }
+//     return f;
+//   }
+
+//   public static void main(String[] args) {
+//       System.out.println(factorial(4));
+//   }
+// }
+
+
+///////////////// binomial ///////////////
+
 public class fun{
 
   public static void printHelloWorld(){
@@ -94,7 +130,17 @@ public class fun{
     return f;
   }
 
+  public static int binCoeff(int n,int r){
+    int fact_n= factorial(n);
+    int fact_r=factorial(r);
+    int fact_nmr=factorial(n-r);
+
+    int binCoeff=fact_n/(fact_r*fact_nmr);
+    return binCoeff;
+  }
+
   public static void main(String[] args) {
-      System.out.println(factorial(4));
+      System.out.println(binCoeff(5, 2));
   }
 }
+
