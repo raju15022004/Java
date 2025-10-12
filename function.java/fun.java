d0179cc5533b57b6ebc0c8df867b18867aa0d398
raju15022004 <raju032004@gmail.com>
2025-10-12 20:18:56 +0530

    fun.java

--- function.java/fun.java
+++ function.java/fun.java
@@ -207,7 +207,22 @@ public class fun{
     System.out.println("decimal of"+myNum+" = "+decNum);
   }
 
+  ////////// Decimal to binary ///////////////
+  public static void decToBin(int n){
+    int myNum=n;
+    int pow=0;
+    int binNum=0;
+
+    while(n>0){
+      int rem=n%2;
+      binNum=binNum+(rem * (int)Math.pow(10,pow));
+      pow++;
+      n=n/2;
+    }
+    System.out.println("binary of"+myNum+" = "+binNum);
+  }
+
   public static void main(String[] args) {
-      binToDec(111);
+      decToBin(7);
   }
 }
