0a3798698b193cb2535051d16ea914ca68714747
raju15022004 <raju032004@gmail.com>
2025-10-14 23:46:09 +0530

    loop.java

--- practice Question/loop.java
+++ practice Question/loop.java
@@ -1,37 +1,60 @@
 
 import java.util.Scanner;
 
-// package practice Question;
 
-public class loop {
+// import java.util.Scanner;
 
+// // package practice Question;
+
+// public class loop {
+
+//   public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+
+//     int number;
+//     int choice;
+//     int evenSum=0;
+//     int oddSum=0;
+//     do {
+//       System.out.print("Enter the number");
+//       number=sc.nextInt();
+
+//       if(number% 2 == 0){
+//         evenSum += number;
+//       }
+//       else{
+//         oddSum +=number;
+//       }
+
+//       System.out.print("D0 you want to continue? press 1 for yes or 0 for no");
+
+//       choice=sc.nextInt();
+
+//     }
+//     while(choice==1);
+//     System.out.println("Sum of even numbers"+evenSum);
+//     System.out.println("Sum of odd numbers"+oddSum);
+
+//   }
+
+// }
+
+
+//////////////////  factorialofanumber  ////////////
+
+public class loop{
   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+      Scanner sc =new Scanner(System.in);
+      int num;
+      int fact = 1;
 
-    int number;
-    int choice;
-    int evenSum=0;
-    int oddSum=0;
-    do {
-      System.out.print("Enter the number");
-      number=sc.nextInt();
+      System.out.print("Enter any positive integer");
+      num = sc.nextInt();
 
-      if(number% 2 == 0){
-        evenSum += number;
+      for(int i=1;i<=num;i++){
+        fact *=i;
       }
-      else{
-        oddSum +=number;
-      }
-
-      System.out.print("D0 you want to continue? press 1 for yes or 0 for no");
-
-      choice=sc.nextInt();
-
-    }
-    while(choice==1);
-    System.out.println("Sum of even numbers"+evenSum);
-    System.out.println("Sum of odd numbers"+oddSum);
-
+      System.out.println("factorial"+fact);
   }
 
 }
