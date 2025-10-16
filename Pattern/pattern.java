534d3f62ec80931de188596c0c4a899dc3b0e159
raju15022004 <raju032004@gmail.com>
2025-10-16 19:03:07 +0530

    pattern.java

---
+++ Pattern/pattern.java
@@ -0,0 +1,22 @@
+public class pattern {
+  public static void hollow_reactangle(int totRows,int totCols){
+    for(int i=1;i<=totRows;i++){
+      for(int j=1;j<=totCols;j++){
+        if(i == 1 || i == totRows || j == 1 || j == totCols){
+          System.out.print("*");
+        }else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+
+    }
+
+
+    }
+    public static void main(String args[]){
+      hollow_reactangle(4, 5);
+
+  }
+
+}
