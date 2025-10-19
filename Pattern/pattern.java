8769a680949811e3eaa7392e9263f4819d8d64d5
raju15022004 <raju032004@gmail.com>
2025-10-19 11:53:56 +0530

    pattern.java

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -23,6 +23,45 @@
 
 ////////////////// half pyramid /////////////
 
+// public class pattern {
+//   public static void hollow_reactangle(int totRows,int totCols){
+//     for(int i=1;i<=totRows;i++){
+//       for(int j=1;j<=totCols;j++){
+//         if(i == 1 || i == totRows || j == 1 || j == totCols){
+//           System.out.print("*");
+//         }else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
+
+//     }
+
+
+//     }
+
+//     public static void inverted_rotated_half_pyramid(int n){
+
+//       for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//            System.out.print(" ");
+//         }
+//           for(int j=1;j<=i;j++){
+//             System.out.print("*");
+//           }
+//           System.out.println();
+//       }
+//     }
+//     public static void main(String args[]){
+//      inverted_rotated_half_pyramid(4);
+
+//   }
+
+// }
+
+
+//////////////// Number /////////
+
 public class pattern {
   public static void hollow_reactangle(int totRows,int totCols){
     for(int i=1;i<=totRows;i++){
@@ -52,8 +91,17 @@ public class pattern {
           System.out.println();
       }
     }
+
+    public static void inverted_rotated_half_pyramid_withNumbers(int n){
+      for(int i=1; i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+          System.out.print(j+" ");
+        }
+        System.out.println();
+      }
+    }
     public static void main(String args[]){
-     inverted_rotated_half_pyramid(4);
+    inverted_rotated_half_pyramid_withNumbers(5);
 
   }
 
