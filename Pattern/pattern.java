2cb8536b37f1ce6fcd1a3ca945372f8822eef259
raju15022004 <raju032004@gmail.com>
2025-10-19 11:38:35 +0530

    pattern.java

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -1,3 +1,28 @@
+// public class pattern {
+//   public static void hollow_reactangle(int totRows,int totCols){
+//     for(int i=1;i<=totRows;i++){
+//       for(int j=1;j<=totCols;j++){
+//         if(i == 1 || i == totRows || j == 1 || j == totCols){
+//           System.out.print("*");
+//         }else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
+
+//     }
+
+
+//     }
+//     public static void main(String args[]){
+//       hollow_reactangle(5,9);
+
+//   }
+
+// }
+
+////////////////// half pyramid /////////////
+
 public class pattern {
   public static void hollow_reactangle(int totRows,int totCols){
     for(int i=1;i<=totRows;i++){
@@ -14,8 +39,21 @@ public class pattern {
 
 
     }
+
+    public static void inverted_rotated_half_pyramid(int n){
+
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+           System.out.print(" ");
+        }
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+          System.out.println();
+      }
+    }
     public static void main(String args[]){
-      hollow_reactangle(5,9);
+     inverted_rotated_half_pyramid(4);
 
   }
 
