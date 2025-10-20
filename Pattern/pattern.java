ac957920b940047a46b024d79a0eea388c8b9fec
raju15022004 <raju032004@gmail.com>
2025-10-20 20:11:38 +0530

    pattern.java

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -196,76 +196,6 @@
 
 //////////////// butterfly /////////////
 
-// public class pattern {
-//   public static void hollow_reactangle(int totRows,int totCols){
-//     for(int i=1;i<=totRows;i++){
-//       for(int j=1;j<=totCols;j++){
-//         if(i == 1 || i == totRows || j == 1 || j == totCols){
-//           System.out.print("*");
-//         }else{
-//           System.out.print(" ");
-//         }
-//       }
-//       System.out.println();
-
-//     }
-//     }
-
-//     public static void butterfly(int n){
-
-//       for(int i=1;i<=n;i++){
-
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         for(int j=1;j<=2*(n-i);j++){
-//           System.out.print(" ");
-//         }
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-
-//       for(int i=n;i>=1;i--){
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         for(int j=1;j<=2*(n-i);j++){
-//           System.out.print(" ");
-//         }
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-//     }
-
-//     public static void solid_rhombus(int n){
-//       for(int i=1;i<=n;i++){
-
-//         for(int j=1;j<=(n-i);j++){
-//           System.out.print(" ");
-//         }
-
-//         for(int j=1;j<=n;j++){
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-//     }
-
-//     public static void main(String args[]){
-//     // butterfly(4);
-//     solid_rhombus(5);
-
-//   }
-
-// }
-
-
-///////////// Diamond /////////////
-
 public class pattern {
   public static void hollow_reactangle(int totRows,int totCols){
     for(int i=1;i<=totRows;i++){
@@ -281,33 +211,103 @@ public class pattern {
     }
     }
 
-    public static void Diamond(int n){
-      // 1st half
+    public static void butterfly(int n){
+
       for(int i=1;i<=n;i++){
 
-        for(int j=1;j<=(n-i);j++){
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        for(int j=1;j<=2*(n-i);j++){
           System.out.print(" ");
         }
-        for(int j=1;j<=(2*i)-1;j++){
+        for(int j=1;j<=i;j++){
           System.out.print("*");
         }
         System.out.println();
       }
 
       for(int i=n;i>=1;i--){
-
-        for(int j=1;j<=(n-i);j++){
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        for(int j=1;j<=2*(n-i);j++){
           System.out.print(" ");
         }
-        for(int j=1;j<=(2*i)-1;j++){
+        for(int j=1;j<=i;j++){
           System.out.print("*");
         }
         System.out.println();
       }
     }
+
+    public static void solid_rhombus(int n){
+      for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=(n-i);j++){
+          System.out.print(" ");
+        }
+
+        for(int j=1;j<=n;j++){
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+
     public static void main(String args[]){
-    Diamond(6);
+    // butterfly(4);
+    solid_rhombus(6);
 
   }
 
 }
+
+
+///////////// Diamond /////////////
+
+// public class pattern {
+//   public static void hollow_reactangle(int totRows,int totCols){
+//     for(int i=1;i<=totRows;i++){
+//       for(int j=1;j<=totCols;j++){
+//         if(i == 1 || i == totRows || j == 1 || j == totCols){
+//           System.out.print("*");
+//         }else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
+
+//     }
+//     }
+
+//     public static void Diamond(int n){
+//       // 1st half
+//       for(int i=1;i<=n;i++){
+
+//         for(int j=1;j<=(n-i);j++){
+//           System.out.print(" ");
+//         }
+//         for(int j=1;j<=(2*i)-1;j++){
+//           System.out.print("*");
+//         }
+//         System.out.println();
+//       }
+
+//       for(int i=n;i>=1;i--){
+
+//         for(int j=1;j<=(n-i);j++){
+//           System.out.print(" ");
+//         }
+//         for(int j=1;j<=(2*i)-1;j++){
+//           System.out.print("*");
+//         }
+//         System.out.println();
+//       }
+//     }
+//     public static void main(String args[]){
+//     Diamond(6);
+
+//   }
+
+// }
