ae4078cfe2e45785fbb12045e4ac628eb23ccd8c
raju15022004 <raju032004@gmail.com>
2025-10-26 19:21:22 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -1,10 +1,21 @@
+
+import java.util.Scanner;
+
 // package Array;
 
 public class array {
   public static void main(String[] args) {
-      int marks[]=new int[50];
+      int marks[]=new int[100];
 
-      int numbers[]={1,2,3};
+     Scanner sc = new Scanner(System.in);
+
+     marks[0]=sc.nextInt();
+     marks[1]=sc.nextInt();
+     marks[2]=sc.nextInt();
+
+     System.out.println("phy :" + marks[0]);
+     System.out.println("chem :" + marks[1]);
+     System.out.println("math :" + marks[2]);
   }
 
 }
