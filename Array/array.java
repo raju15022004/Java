d045cfc750a0f562e41ac76132e2108be03542e0
raju15022004 <raju032004@gmail.com>
2025-10-26 20:18:35 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -18,20 +18,48 @@
 // }
 
 
+// public class array{
+//   public static void update(int marks[]){
+//     for(int i=0;i<marks.length;i++){
+//       marks[i]=marks[i]+i;
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//       int marks[]={97,98,99};
+//       update(marks);
+
+//       for(int i=0;i<marks.length;i++){
+//         System.out.print( marks[i]+" ");
+//       }
+//       System.out.println();
+//   }
+// }
+
+
+////////////// largest ///////
+
 public class array{
-  public static void update(int marks[]){
-    for(int i=0;i<marks.length;i++){
-      marks[i]=marks[i]+i;
-    }
+  public static int  getLargest(int numbers[]) {
+      int largest = Integer.MIN_VALUE;
+      int smallest=Integer.MAX_VALUE;
+
+      for(int i=0;i<numbers.length;i++){
+        if(largest<numbers[i]){
+          largest=numbers[i];
+
+        }
+        if(smallest>numbers[i]){
+          smallest=numbers[i];
+        }
+      }
+      System.out.println("samllest value is :"+smallest);
+      return largest;
   }
 
   public static void main(String[] args) {
-      int marks[]={97,98,99};
-      update(marks);
-
-      for(int i=0;i<marks.length;i++){
-        System.out.print( marks[i]+" ");
-      }
-      System.out.println();
+      int numbers[]={1,2,6,3,5};
+      System.out.println("largest value is:"+getLargest(numbers));
   }
 }
+
