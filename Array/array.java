b13a73cbb89fdcb9a9d9e06f9a84b2a949eb4c4e
raju15022004 <raju032004@gmail.com>
2025-10-26 19:47:24 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -1,21 +1,37 @@
 
-import java.util.Scanner;
 
-// package Array;
+// public class array {
+//   public static void main(String[] args) {
+//       int marks[]=new int[100];
 
-public class array {
-  public static void main(String[] args) {
-      int marks[]=new int[100];
+//      Scanner sc = new Scanner(System.in);
 
-     Scanner sc = new Scanner(System.in);
+//      marks[0]=sc.nextInt();
+//      marks[1]=sc.nextInt();
+//      marks[2]=sc.nextInt();
 
-     marks[0]=sc.nextInt();
-     marks[1]=sc.nextInt();
-     marks[2]=sc.nextInt();
+//      System.out.println("phy :" + marks[0]);
+//      System.out.println("chem :" + marks[1]);
+//      System.out.println("math  :" + marks[2]);
+//   }
 
-     System.out.println("phy :" + marks[0]);
-     System.out.println("chem :" + marks[1]);
-     System.out.println("math :" + marks[2]);
+// }
+
+
+public class array{
+  public static void update(int marks[]){
+    for(int i=0;i<marks.length;i++){
+      marks[i]=marks[i]+i;
+    }
   }
 
+  public static void main(String[] args) {
+      int marks[]={97,98,99};
+      update(marks);
+
+      for(int i=0;i<marks.length;i++){
+        System.out.print( marks[i]+" ");
+      }
+      System.out.println();
+  }
 }
