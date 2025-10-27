7170d63b1bdb841975c6fc4d416396a79862a10d
raju15022004 <raju032004@gmail.com>
2025-10-27 22:51:37 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -95,28 +95,47 @@
 
 ///////////////// revers ////////////
 
+// public class array{
+//   public static void reverse(int numbers[]){
+//     int first=0,last=numbers.length-1;
+
+//     while(first<last){
+//       int temp=numbers[last];
+//       numbers[last]=numbers[first];
+//       numbers[first]=temp;
+
+//       first++;
+//       last--;
+
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//       int numbers[]={2,4,6,8,10};
+
+//       reverse(numbers);
+//       for(int i=0;i<numbers.length;i++){
+//         System.out.print (numbers[i]+" ");
+//       }
+//       System.out.println();
+//   }
+// }
+
+//////////////// pairs ///////////////////
+
 public class array{
-  public static void reverse(int numbers[]){
-    int first=0,last=numbers.length-1;
-
-    while(first<last){
-      int temp=numbers[last];
-      numbers[last]=numbers[first];
-      numbers[first]=temp;
-
-      first++;
-      last--;
-
+  public static void printPairs(int numbers[]){
+    for(int i=0;i<numbers.length;i++){
+      int curr=numbers[i];
+      for(int j=i+1;j<numbers.length;j++){
+        System.out.print("("+ curr + "," + numbers[j] + ") ");
+      }
+      System.out.println();
     }
   }
 
   public static void main(String[] args) {
       int numbers[]={2,4,6,8,10};
-
-      reverse(numbers);
-      for(int i=0;i<numbers.length;i++){
-        System.out.print(numbers[i]+" ");
-      }
-      System.out.println();
+      printPairs(numbers);
   }
 }
