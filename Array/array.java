ffe2712ec785b68a42f332f49a6fd41887ba6a8e
raju15022004 <raju032004@gmail.com>
2025-10-27 22:35:08 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -65,29 +65,58 @@
 
 ////////////// bineary search ////////////////
 
- public class array{
+//  public class array{
 
-  public static int binearySearch(int numbers[],int key){
-    int start=0,end=numbers.length-1;
+//   public static int binearySearch(int numbers[],int key){
+//     int start=0,end=numbers.length-1;
 
-    while(start <= end){
-      int mid = (start + end)/2;
+//     while(start <= end){
+//       int mid = (start + end)/2;
 
-      if(numbers[mid] == key){
-        return mid;
-      }
-      if(numbers[mid]<key){
-        start=mid+1;
-      }
-      else{
-        end=mid-1;
-      }
+//       if(numbers[mid] == key){
+//         return mid;
+//       }
+//       if(numbers[mid]<key){
+//         start=mid+1;
+//       }
+//       else{
+//         end=mid-1;
+//       }
+//     }
+//     return-1;
+//   }
+//   public static void main(String[] args) {
+//       int numbers[]={2,4,6,8,10,12,14};
+//       int key=10;
+//       System.out.println("index for key is :"+binearySearch(numbers, key));
+//   }
+//  }
+
+
+///////////////// revers ////////////
+
+public class array{
+  public static void reverse(int numbers[]){
+    int first=0,last=numbers.length-1;
+
+    while(first<last){
+      int temp=numbers[last];
+      numbers[last]=numbers[first];
+      numbers[first]=temp;
+
+      first++;
+      last--;
+
     }
-    return-1;
   }
+
   public static void main(String[] args) {
-      int numbers[]={2,4,6,8,10,12,14};
-      int key=10;
-      System.out.println("index for key is :"+binearySearch(numbers, key));
+      int numbers[]={2,4,6,8,10};
+
+      reverse(numbers);
+      for(int i=0;i<numbers.length;i++){
+        System.out.print(numbers[i]+" ");
+      }
+      System.out.println();
   }
- }
+}
