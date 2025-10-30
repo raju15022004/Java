5e02ba938eaee302ba21d32f82ecba5b5c898bf6
raju15022004 <raju032004@gmail.com>
2025-10-30 22:03:58 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -124,18 +124,25 @@
 //////////////// pairs ///////////////////
 
 public class array{
-  public static void printPairs(int numbers[]){
+  public static void printSubarrays(int numbers[]){
+    int ts=0;
     for(int i=0;i<numbers.length;i++){
-      int curr=numbers[i];
-      for(int j=i+1;j<numbers.length;j++){
-        System.out.print("("+ curr + "," + numbers[j] + ") ");
+      int start=i;
+      for(int j=i;j<numbers.length;j++){
+        int end=j;
+        for(int k=start;k<end;k++){
+          System.out.print(numbers[k]+" ");
+        }
+        ts++;
+        System.out.println();
       }
       System.out.println();
     }
-  }
+    System.out.println("total subarrays="+ts);
 
+  }
   public static void main(String[] args) {
       int numbers[]={2,4,6,8,10};
-      printPairs(numbers);
+      printSubarrays(numbers);
   }
 }
