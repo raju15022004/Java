d5cb1eb4229d2258ab5ab8e9a3171cb3c6163af7
raju15022004 <raju032004@gmail.com>
2025-10-31 21:57:35 +0530

    array.java

--- Array/array.java
+++ Array/array.java
@@ -142,7 +142,7 @@ public class array{
 
   }
   public static void main(String[] args) {
-      int numbers[]={2,4,6,8,10,12};
+      int numbers[]={2,4,6,8,10};
       printSubarrays(numbers);
   }
 }
