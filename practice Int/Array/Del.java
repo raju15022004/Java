a808b3493c14ddc859078fab8f1e76176aac441f
raju15022004 <raju032004@gmail.com>
2025-11-08 22:46:58 +0530

    Del.java

---
+++ practice Int/Array/Del.java
@@ -0,0 +1,16 @@
+public class Del {
+  public static void main(String[] args) {
+      int[] arr={10,20,30,40,50};
+      int index=2;
+
+      for(int i=index;i<arr.length-1;i++){
+        arr[i]=arr[i++];
+      }
+      System.out.println("Array After deletion:");
+      for(int i=0;i<arr.length-1;i++){
+        System.out.println(arr[i]+" ");
+      }
+
+  }
+
+}
