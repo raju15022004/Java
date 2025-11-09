8f90372f32f73a73fb3187f5fb7d3a8269865661
raju15022004 <raju032004@gmail.com>
2025-11-09 17:49:33 +0530

    max.java

--- practice Int/Array/Sum.java
+++ practice Int/Array/Sum.java
@@ -1,26 +1,25 @@
-// // package practice Int.Array;
+// package practice Int.Array;
 
-// public class Sum {
-//   public static void main(String[] args) {
-//       int[] arr={2,7,4,5,9,1,3};
-//       int sum=6;
+public class Sum {
+  public static void main(String[] args) {
+      int[] arr={2,7,4,5,9,1,3};
+      int sum=0;
 
-//       System.out.println("jodiya jinka total"+sum+"hai");
+      System.out.println("jodiya jinka total"+sum+"hai");
 
-//       for(int i=0;i<arr.length;i++){
-//         for(int j=i+1;j<arr.length;j++){
-//           if(arr[i]+arr[j]==sum){
-//             System.out.println(arr[i]+"+"+arr[j]+"="+sum);
-//           }
-//         }
-//       }
-//   }
+      for(int i=0;i<arr.length;i++){
+        for(int j=i+1;j<arr.length;j++){
+          if(arr[i]+arr[j]==sum){
+            System.out.println(arr[i]+"+"+arr[j]+"="+sum);
+          }
+        }
+      }
+  }
 
-// }
+}
 
 
 
-//////////////////// factorial //////////////////
 
 
 
---
+++ practice Int/Array/max.java
@@ -0,0 +1,18 @@
+public class max {
+  public static void main(String[] args) {
+      int[] arr={10,20,90,40,70};
+
+      int min=arr[0];
+
+      for(int i=1;i<arr.length;i++){
+        if(arr[i]<min){
+           min=arr[i];
+        }
+      }
+        System.out.println("minmum number:="+min);
+
+
+
+  }
+
+}
