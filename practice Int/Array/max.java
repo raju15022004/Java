4eaf57df894926e059fb6df4b5e699dbf3ae2320
raju15022004 <raju032004@gmail.com>
2025-11-17 18:16:24 +0530

    max.java

--- practice Int/Array/max.java
+++ practice Int/Array/max.java
@@ -2,7 +2,8 @@ public class max {
   public static void main(String[] args) {
       int[] arr={10,20,90,40,70};
 
-      int min=arr[2];
+      int min=arr[4];
+      
 
       for(int i=1;i<arr.length;i++){
         if(arr[i]<min){
