774827d662ea26cc52048c4e15756d8958d92b97
raju15022004 <raju032004@gmail.com>
2025-12-07 19:36:18 +0530

    reverse.java

--- practice Int/Array/reverse.java
+++ practice Int/Array/reverse.java
@@ -1,6 +1,6 @@
 public class reverse {
   public static void main(String[] args) {
-      int[] arr={10,20,30,40,50,60,70};
+      int[] arr={10,20,30,40,50,60,70,80};
 
       int start=0;
       int end=arr.length-1;
