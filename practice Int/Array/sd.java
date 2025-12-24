9db3476e797883e999bea2fd004ae4d144aa5af2
raju15022004 <raju032004@gmail.com>
2025-12-24 10:58:03 +0530

    sd.java

---
+++ practice Int/Array/sd.java
@@ -0,0 +1,14 @@
+
+  import java.util.Scanner;
+
+public class sd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a long number: ");
+        long num = sc.nextLong();
+
+        System.out.println("You entered: " + num);
+    }
+}
+
