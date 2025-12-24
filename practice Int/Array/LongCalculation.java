3d6cc36a6083b8a2cd789f13dbc4cfd20888df87
raju15022004 <raju032004@gmail.com>
2025-12-24 10:51:19 +0530

    longCalculation.java

---
+++ practice Int/Array/LongCalculation.java
@@ -0,0 +1,14 @@
+
+public class LongCalculation {
+    public static void main(String[] args) {
+        long a = 1000000000L;
+        long b = 2000000000L;
+
+        long sum = a + b;
+        long product = a * b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+    }
+}
+
