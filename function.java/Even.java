49d849e551607e2f4e74187b1cf6e76488596a1d
raju15022004 <raju032004@gmail.com>
2025-12-31 19:48:57 +0530

    Even.java

--- function.java/Even.java
+++ function.java/Even.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Even {
     public static boolean isEven(int num){
-        return num%2==0;
+        return num%4==0;
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
         System.out.print("Enter a number:");
         int number=scanner.nextInt();
-    
+
 
     if(isEven(number)){
         System.out.println(number+"is even.");
@@ -20,6 +20,6 @@ public class Even {
         System.out.println(number+"is odd.");
     }
     scanner.close();
-    
+
 }
 }
