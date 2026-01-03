3ee2d1d394eaf197e63d8011748f3cd9f680ca9a
raju15022004 <raju032004@gmail.com>
2026-01-03 20:51:34 +0530

    pattern.java

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -23,41 +23,41 @@
 
 ////////////////// half pyramid /////////////
 
-public class pattern {
-  public static void hollow_reactangle(int totRows,int totCols){
-    for(int i=1;i<=totRows;i++){
-      for(int j=1;j<=totCols;j++){
-        if(i == 1 || i == totRows || j == 1 || j == totCols){
-          System.out.print("*");
-        }else{
-          System.out.print(" ");
-        }
-      }
-      System.out.println();
+// public class pattern {
+//   public static void hollow_reactangle(int totRows,int totCols){
+//     for(int i=1;i<=totRows;i++){
+//       for(int j=1;j<=totCols;j++){
+//         if(i == 1 || i == totRows || j == 1 || j == totCols){
+//           System.out.print("*");
+//         }else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
 
-    }
+//     }
 
 
-    }
+//     }
 
-    public static void inverted_rotated_half_pyramid(int n){
+//     public static void inverted_rotated_half_pyramid(int n){
 
-      for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-           System.out.print(" ");
-        }
-          for(int j=1;j<=i;j++){
-            System.out.print("*");
-          }
-          System.out.println();
-      }
-    }
-    public static void main(String args[]){
-     inverted_rotated_half_pyramid(8);
+//       for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//            System.out.print(" ");
+//         }
+//           for(int j=1;j<=i;j++){
+//             System.out.print("*");
+//           }
+//           System.out.println();
+//       }
+//     }
+//     public static void main(String args[]){
+//      inverted_rotated_half_pyramid(8);
 
-  }
+//   }
 
-}
+// }
 
 
 //////////////// Number /////////
@@ -160,39 +160,39 @@ public class pattern {
 ///////////////////////// 0 1 triangle ///////////
 
 
-// public class pattern {
-//   public static void hollow_reactangle(int totRows,int totCols){
-//     for(int i=1;i<=totRows;i++){
-//       for(int j=1;j<=totCols;j++){
-//         if(i == 1 || i == totRows || j == 1 || j == totCols){
-//           System.out.print("*");
-//         }else{
-//           System.out.print(" ");
-//         }
-//       }
-//       System.out.println();
+public class pattern {
+  public static void hollow_reactangle(int totRows,int totCols){
+    for(int i=1;i<=totRows;i++){
+      for(int j=1;j<=totCols;j++){
+        if(i == 1 || i == totRows || j == 1 || j == totCols){
+          System.out.print("*");
+        }else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
 
-//     }
-//     }
+    }
+    }
 
-//     public static void zero_one_triangle(int n){
-//       for(int i=1;i<=n;i++){
-//         for(int j=1;j<=i;j++){
-//           if((i+j)%2 == 0){
-//             System.out.print("1");
-//           }else{
-//             System.out.print("0");
-//           }
-//         }
-//         System.out.println();
-//       }
-//     }
-//     public static void main(String args[]){
-//       zero_one_triangle(5);
+    public static void zero_one_triangle(int n){
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+          if((i+j)%2 == 0){
+            System.out.print("1");
+          }else{
+            System.out.print("0");
+          }
+        }
+        System.out.println();
+      }
+    }
+    public static void main(String args[]){
+      zero_one_triangle(6);
 
-//   }
+  }
 
-// }
+}
 
 //////////////// butterfly /////////////
 
