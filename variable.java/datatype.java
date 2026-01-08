124b1b3508b7daafa5895c1186be96eb84e0e1a3
raju15022004 <raju032004@gmail.com>
2026-01-08 19:19:34 +0530

    datatype.java

--- variable.java/datatype.java
+++ variable.java/datatype.java
@@ -2,16 +2,17 @@
 
 public class datatype {
     public static void main(String[] args) {
-        byte b=8;
-        System.out.println(b);
-        char ch='a';
-        System.out.println(ch);
-        boolean var=false;
-        System.out.println(var);
-        float price=25;
-        System.out.println(price);
-        short n=240;
-        System.out.println(n);
+       byte b=8;
+       System.out.println(b);
+       char ch='a';
+       System.out.println(ch);
+      boolean var =false;
+      System.out.println(var);
+      float price = 10;
+      System.out.println(price);
+      int number=25;
+      System.out.println(number);
+
     }
-    
+
 }
