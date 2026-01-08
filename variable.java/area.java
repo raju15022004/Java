dbbbbc2e819109f10ea75a0388144e3bc25059ec
raju15022004 <raju032004@gmail.com>
2026-01-08 19:04:01 +0530

    variable.java

--- variable.java/area.java
+++ variable.java/area.java
@@ -1,12 +1,16 @@
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class area {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        float rad=sc.nextFloat();
-        float area=3.14f*rad*rad;
-        System.out.println(area);
-    }
-    
+// public class area {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         float rad=sc.nextFloat();
+//         float area=3.14f*rad*rad;
+//         System.out.println(area);
+//     }
+
+// }
+
+public class area{
+  
 }
--- variable.java/variable.java
+++ variable.java/variable.java
@@ -2,15 +2,12 @@
 
 public class variable {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        int a=10;
+        int b=5;
         System.out.println(a);
         System.out.println(b);
-        String name = "tony stack";
+        String name="Raju";
         System.out.println(name);
-
-        a=50;
-        System.out.println(a);
     }
-    
+
 }
