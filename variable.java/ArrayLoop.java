a21b0789b3ea841f257de481066a01d2f872dbf2
raju15022004 <raju032004@gmail.com>
2026-01-09 20:02:30 +0530

    ArrayLoop.java

---
+++ variable.java/ArrayLoop.java
@@ -0,0 +1,12 @@
+
+public class ArrayLoop {
+   public static void main(String[] args) {
+
+        int[] arr = {5, 10, 15, 20};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
