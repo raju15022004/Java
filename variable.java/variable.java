da8b67545a23b653699ada00141fbbb628e7fbcb
raju15022004 <raju032004@gmail.com>
2026-01-12 21:11:16 +0530

    variable.java

--- variable.java/variable.java
+++ variable.java/variable.java
@@ -6,7 +6,7 @@ public class variable {
         int b=5;
         System.out.println(a);
         System.out.println(b);
-        String name="Raju kumar Sahani";
+        String name="Raju kumar va";
         System.out.println(name);
     }
 
