cae946147843b7dfba920c4eff7ae3c20effa091
raju15022004 <raju032004@gmail.com>
2026-01-17 20:31:34 +0530

    ForEachArra.java

---
+++ pattern.java/ForEachArray.java
@@ -0,0 +1,9 @@
+public class ForEachArray {
+      public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8};
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
