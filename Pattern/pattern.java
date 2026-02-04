665baac318572c7f6fc23e229e7916ad98539d17
raju15022004 <raju032004@gmail.com>
2026-02-04 18:24:25 +0530

    pattern.java

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -160,42 +160,6 @@
 ///////////////////////// 0 1 triangle ///////////
 
 
-public class pattern {
-  public static void hollow_reactangle(int totRows,int totCols){
-    for(int i=1;i<=totRows;i++){
-      for(int j=1;j<=totCols;j++){
-        if(i == 1 || i == totRows || j == 1 || j == totCols){
-          System.out.print("*");
-        }else{
-          System.out.print(" ");
-        }
-      }
-      System.out.println();
-
-    }
-    }
-
-    public static void zero_one_triangle(int n){
-      for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-          if((i+j)%2 == 0){
-            System.out.print("1");
-          }else{
-            System.out.print("0");
-          }
-        }
-        System.out.println();
-      }
-    }
-    public static void main(String args[]){
-      zero_one_triangle(9);
-
-  }
-
-}
-
-//////////////// butterfly /////////////
-
 // public class pattern {
 //   public static void hollow_reactangle(int totRows,int totCols){
 //     for(int i=1;i<=totRows;i++){
@@ -211,58 +175,94 @@ public class pattern {
 //     }
 //     }
 
-//     public static void butterfly(int n){
-
+//     public static void zero_one_triangle(int n){
 //       for(int i=1;i<=n;i++){
-
 //         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         for(int j=1;j<=2*(n-i);j++){
-//           System.out.print(" ");
-//         }
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-
-//       for(int i=n;i>=1;i--){
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
-//         }
-//         for(int j=1;j<=2*(n-i);j++){
-//           System.out.print(" ");
-//         }
-//         for(int j=1;j<=i;j++){
-//           System.out.print("*");
+//           if((i+j)%2 == 0){
+//             System.out.print("1");
+//           }else{
+//             System.out.print("0");
+//           }
 //         }
 //         System.out.println();
 //       }
 //     }
-
-//     public static void solid_rhombus(int n){
-//       for(int i=1;i<=n;i++){
-
-//         for(int j=1;j<=(n-i);j++){
-//           System.out.print(" ");
-//         }
-
-//         for(int j=1;j<=n;j++){
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-//     }
-
 //     public static void main(String args[]){
-//     // butterfly(4);
-//     solid_rhombus(9);
-
+//       zero_one_triangle(9);
 
 //   }
 
 // }
+
+//////////////// butterfly /////////////
+
+public class pattern {
+  public static void hollow_reactangle(int totRows,int totCols){
+    for(int i=1;i<=totRows;i++){
+      for(int j=1;j<=totCols;j++){
+        if(i == 1 || i == totRows || j == 1 || j == totCols){
+          System.out.print("*");
+        }else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+
+    }
+    }
+
+    public static void butterfly(int n){
+
+      for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        for(int j=1;j<=2*(n-i);j++){
+          System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+
+      for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        for(int j=1;j<=2*(n-i);j++){
+          System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+
+    public static void solid_rhombus(int n){
+      for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=(n-i);j++){
+          System.out.print(" ");
+        }
+
+        for(int j=1;j<=n;j++){
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+
+    public static void main(String args[]){
+    // butterfly(4);
+    solid_rhombus(7);
+
+
+  }
+
+}
 
 
 ///////////// Diamond /////////////
